1a3b31776d443d9b068ce94e32483f08fec4a79a
vaibhav1510 <vaibhav@chargebee.com>
2020-02-19 19:45:35 +0530

    removed duplicates

--- exp-app/src/java/com/expenses/app/config/Env.java
+++
@@ -1,9 +0,0 @@
-package com.expenses.app.config;
-
-import java.io.File;
-
-public class Env {
-
-	public static final String DB_PATH = ".." + File.separator + "db";
-
-}
--- exp-app/src/java/com/expenses/app/utils/StringUtils.java
+++
@@ -1,8 +0,0 @@
-package com.expenses.app.utils;
-
-public class StringUtils {
-
-    public static Boolean isNUllOrEmpty(String s) {
-        return s == null || s.length() == 0;
-    }
-}
